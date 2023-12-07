ff8c2ac5c1e41a225e5a0e987b9d3b5db89ad7b8
samuel-bazinet <bazinet.samuel1@gmail.com>
2023-12-06 22:03:47 -0500

    Ran formatter and addressed some IDE concerns for the remaining files in root

--- audioscribe/lib/app_constants.dart
+++ audioscribe/lib/app_constants.dart
@@ -1,19 +1,12 @@
 import 'dart:ui';
 
-enum AuthMode {
-	LOGIN,
-	SIGNUP,
-	FORGOTPASSWORD
-}
+enum AuthMode { LOGIN, SIGNUP, FORGOTPASSWORD }
 
-enum AppPage {
-	HOME,
-	COLLECTION
-}
+enum AppPage { HOME, COLLECTION }
 
 class AppColors {
-	static const Color primaryAppColor = Color(0xFF524178);
-	static const Color secondaryAppColor = Color(0xFF242424);
-	static const Color primaryAppColorBrighter = Color(0xFF9260FC);
-	static const Color backgroundColor1 = Color(0xFF303030);
+  static const Color primaryAppColor = Color(0xFF524178);
+  static const Color secondaryAppColor = Color(0xFF242424);
+  static const Color primaryAppColorBrighter = Color(0xFF9260FC);
+  static const Color backgroundColor1 = Color(0xFF303030);
 }
--- audioscribe/lib/main.dart
+++ audioscribe/lib/main.dart
@@ -5,35 +5,29 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-	WidgetsFlutterBinding.ensureInitialized();
-	await Firebase.initializeApp(
-		options: DefaultFirebaseOptions.currentPlatform,
-	);
-	
-	// initialize notification
-	await NotificationService.initialize();
-	NotificationService.sendRandomBookRecommendation();
-	
-	runApp(const MyApp());
+  // initialize notification
+  await NotificationService.initialize();
+  NotificationService.sendRandomBookRecommendation();
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-	const MyApp({super.key});
+  const MyApp({super.key});
 
-	// This widget is the root of your application.
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			debugShowCheckedModeBanner: false,
-			home: const AuthPage(),
-			theme: ThemeData(
-				popupMenuTheme: const PopupMenuThemeData(
-					color: Color(0xFF242424)
-				)
-			),
-		);
-	}
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const AuthPage(),
+      theme: ThemeData(
+          popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF242424))),
+    );
+  }
 }
-
-
